40563bf9e9d9d12de4b760adfa1603b16d19f835
felipepoty <39703388+felipepoty@users.noreply.github.com>
2018-05-28 11:39:43 -0300

    Create etapa1.js

---
+++ etapa1.js
@@ -0,0 +1,17 @@
+var x = 0
+function setup() {
+  createCanvas(300, 300);
+}
+
+function draw() {
+  background(50);
+  ellipse(20, 160, 40, 20)
+  
+  rect(280, 150,20,20)
+  
+  fill(255);
+  
+
+  fill(200, 200, 200);
+ 
+}
