51c98423a5eb50ad0fa64b1e2782d2ad0d7d96df
felipepoty <39703388+felipepoty@users.noreply.github.com>
2018-05-28 18:01:59 -0300

    Update etapa4.js

--- etapa4.js
+++ etapa4.js
@@ -1,45 +1,60 @@
 
-var x =20
-var y = 0
-var dy= 5
+var x =10
+var qy = 160
+var y = 160
+var u= 5
 var dt=5
-var t=40
+var dx=x
+var disparo = false
+var dy= y
+
 function setup() {
   createCanvas(300, 300);
   frameRate(30);
+  x=10
 }
 
 function keyPressed(){
-  if(keyCode  === RIGHT_ARROW){
-  t=t+dt
+  if((keyCode  === CONTROL)&&(!disparo)){
+  disparo=true
+  dx= x
+  dy= y
   }
 }
 
 function draw() {
   background(50);
-  rect(150,y,10,40);
-  y= y+dy
-  if(y>height)
-  {y = random(-500,50)
+  rect(150,qy,10,40);
+  qy= qy+u
+  if(disparo){
+    dx= dx+10
+  }
+  if(dx>width){
+    disparo = false
+  }
+  
+  if(qy>height)
+  {qy = random(-500,0)
   }
 
-  ellipse(x, 160, 40, 20);
-  if(keyIsDown(RIGHT_ARROW))
-  { x += 3
+  ellipse(x, y, 20, 40)
+  fill(200,20,90);
+  if(keyIsDown(UP_ARROW))
+  { y += 3
   }
-  if(keyIsDown(LEFT_ARROW))
-  {x -= 3
+  if(keyIsDown(DOWN_ARROW))
+  {y -= 3
   }
-  rect(t,150,10,10)
+  ellipse(dx,dy,10,5)
 
   
   
-  rect(280, 150,20,20);
+  rect(280, 150,20,20)
+  fill(20,200,80);
   
   fill(255);
   
 
-  fill(200, 200, 200);
- 
+
 }
 
